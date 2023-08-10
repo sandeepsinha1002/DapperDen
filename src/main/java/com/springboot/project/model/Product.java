01f62a315bbbb8696398c42e74233479e10f2f69
Sandeep Sinha <sinhasandy345@gmail.com>
2023-08-10 17:41:16 +0530

    changes

--- src/main/java/com/springboot/project/model/Product.java
+++ src/main/java/com/springboot/project/model/Product.java
@@ -1,14 +1,22 @@
 package com.springboot.project.model;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Locale.Category;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 @Entity
@@ -29,11 +37,76 @@ public class Product {
     @Column(name = "price")
     private int price;
 
+    @Column(name = "brand")
+    private String brand;
+
     @Column(name = "image_url")
     private String imageUrl;
 
-    @OneToMany(mappedBy = "product" , cascade = CascadeType.ALL,orphanRemoval = true)
+    @Embedded
+    @ElementCollection
+    @Column(name = "sizes")
+    private Set<Size> sizes = new HashSet<>();
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews = new ArrayList<>();
+
+    @ManyToOne()
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private LocalDateTime createdAt;
+
+    @Column(name = "num_ratings")
+    private int numRatings;
+
+    public Product(Long id, String title, String description, int quantity, int price, String brand, String imageUrl,
+            Set<Size> sizes, List<Review> reviews, Category category, LocalDateTime createdAt, int numRatings) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+        this.brand = brand;
+        this.imageUrl = imageUrl;
+        this.sizes = sizes;
+        this.reviews = reviews;
+        this.category = category;
+        this.createdAt = createdAt;
+        this.numRatings = numRatings;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Set<Size> getSizes() {
+        return sizes;
+    }
+
+    public void setSizes(Set<Size> sizes) {
+        this.sizes = sizes;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
     public Long getId() {
         return id;
@@ -98,8 +171,5 @@ public class Product {
     public void setNumRatings(int numRatings) {
         this.numRatings = numRatings;
     }
-
-    @Column(name = "num_ratings")
-    private int numRatings;
 
 }
